b7f528ae426ac484331e1038b47426aa755046a1
nuyos <id2ana@naver.com>
2025-09-10 20:46:39 +0000

    first deploy

---
+++ content/hello.md
@@ -0,0 +1,7 @@
+Title: 뇨스의 개발 일기장
+Date: 2025-09-11
+Category: 블로그
+Tags: pelican, python
+Slug: hello-world
+
+안녕하세요! Pelican을 사용해 만든 뇨스의 개발 일기장 입니다 😎
---
+++ pelicanconf.py
@@ -0,0 +1,9 @@
+AUTHOR = '뇨스'
+SITENAME = '뇨스의 개발 일기장'
+SITEURL = 'https://nuyos.github.io'
+
+PATH = 'content'
+TIMEZONE = 'Asia/Seoul'
+DEFAULT_LANG = 'ko'
+
+DEFAULT_PAGINATION = 10
